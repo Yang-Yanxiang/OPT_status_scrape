ba392a35a5bb971b54dc5d1c0de35f81b66caae6
Yang-Yanxiang <leoyyx2009@gmail.com>
2019-07-13 10:22:12 -0500

    update

--- main.py
+++ main.py
@@ -8,24 +8,39 @@ import time
 #New Card Is Being Produced
 #Card Was Mailed To Me
 #Card Was Picked Up By The United States Postal Service
-candidates = set(['Case Was Received', 'Card Was Delivered To Me By The Post Office', 'New Card Is Being Produced', 'Card Was Mailed To Me', 'Card Was Picked Up By The United States Postal Service'])
+candidates = set(['Card Was Delivered To Me By The Post Office', 'New Card Is Being Produced', 'Card Was Mailed To Me', 'Card Was Picked Up By The United States Postal Service', 'Case Was Approved'])
+other = set(['', ' ', 'Case Rejected Because I Sent An Incorrect Fee', 'Fees Were Waived', 'Request for Initial Evidence Was Mailed', 'Fingerprint Fee Was Received'])
+pending ='Case Was Received'
 my_case_num = 1990232789
 url_prefix = 'https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum=YSC'
 count_total = 0
 count_valid = 0
-for i in range(my_case_num - 2000, my_case_num + 2000):
+count_other = 0
+count_pending = 0
+step = 0
+for i in range(1990232500, 1990233000):
     url = url_prefix + str(i)
     response = requests.get(url)
     bs = BeautifulSoup(response.text, 'html.parser')
     temp = bs.find(class_='col-lg-12 appointment-sec center')
+    if not temp:
+        print("error")
+        time.sleep(0.2)
+        continue
     content = temp.find('p').get_text()
     status = temp.find('h1').get_text()
-    if 'I-765' in content:
-        count_total += 1
-        if status in candidates:
-            count_valid += 1
-    time.sleep(1)
-print(count_total)
-print(count_valid)
-print(count_valid / count_total * 100)
 
+    if status in candidates:
+        count_valid += 1
+    elif status == pending:
+        count_pending += 1
+    else:
+        count_other += 1
+    time.sleep(0.2)
+    step += 1
+    print(step, status)
+print('total:', step)
+print('total I-765:', count_total)
+print('finished:', count_valid)
+print('pending:', count_pending)
+print('other:', count_other)
